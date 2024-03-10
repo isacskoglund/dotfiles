6d3fc3c8e3a637157a7e9229a67610483405ed9d
isacskoglund <isacskoglund02@gmail.com>
2024-03-10 08:14:50 +0000

    Increase signcolumn size.

--- .config/nvim/lua/core/settings.lua
+++ .config/nvim/lua/core/settings.lua
@@ -5,6 +5,7 @@ vim.keymap.set("n", "<leader>o", "o<Esc>k")
 -- insert new line above without moving cursor or leaving normal mode
 vim.keymap.set("n", "<leader>O", "O<Esc>j")
 
+vim.o.signcolumn = "yes:2"
 vim.wo.number = true
 vim.wo.relativenumber = true
 vim.api.nvim_set_hl(0, "LineNr", { fg = "#8591a6" })
