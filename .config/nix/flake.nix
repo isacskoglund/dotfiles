c75040458e023f11ee22204687c3fe0f20461d69
Isac Skoglund <isacskoglund02@gmail.com>
2024-11-16 22:57:05 +0100

    Save.

--- .config/nix/flake.nix
+++ .config/nix/flake.nix
@@ -12,28 +12,15 @@
   outputs = inputs@{ self, nixpkgs, home-manager, nix-darwin, ... }:
   let
     configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ pkgs.vim
-        ];
-
-      # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
-
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
       programs.zsh.enable = true;
-
-      # Set Git commit hash for darwin-version.
       system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
       system.stateVersion = 5;
-
-      # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "aarch64-darwin";
+
+      users.users.isacskoglund.home = "/Users/isacskoglund";
+      nix.configureBuildUsers = true;
+      nix.useDaemon = true;
     };
   in
   {
@@ -42,9 +29,6 @@
     darwinConfigurations."Isacs-MacBook-Air" = nix-darwin.lib.darwinSystem {
       modules = [
         configuration 
-        {
-          users.users.isacskoglund.home = "/Users/isacskoglund";
-        }
         home-manager.darwinModules.home-manager {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
@@ -54,6 +38,6 @@
     };
 
     # Expose the package set, including overlays, for convenience.
-    # darwinPackages = self.darwinConfigurations."Isacs-MacBook-Air".pkgs;
+    darwinPackages = self.darwinConfigurations."Isacs-MacBook-Air".pkgs;
   };
 }
