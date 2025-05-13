b84a9d831e6449414659b7267928c55d67219897
Isac Skoglund <isacskoglund02@gmail.com>
2025-05-13 10:21:21 +0200

    Accept autosuggestions with ctrl space.

--- .zshrc.extra.sh
+++ .zshrc.extra.sh
@@ -17,7 +17,7 @@ bindkey -M menuselect 'k' vi-up-line-or-history
 bindkey -M menuselect 'l' vi-forward-char
 bindkey -M menuselect 'j' vi-down-line-or-history
 bindkey '^?' backward-delete-char
-bindkey '^ ' autosuggest-accept
+bindkey -M viins '^@' autosuggest-accept
 
 # Cursor style
 function zle-keymap-select {
