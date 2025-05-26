9cb0cebae5c2e61426ddcbf4c2e2b27797f42824
Isac Skoglund <isacskoglund02@gmail.com>
2025-05-26 18:55:08 +0200

    Cleanup.

--- scripts/macos-setup.sh
+++
@@ -1,6 +0,0 @@
-# Increase KeyRepeat beyond max
-defaults write -g InitialKeyRepeat -int 10 
-defaults write -g KeyRepeat -int 1
-
-# Enable touch id for sudo
-echo "auth sufficient pam_tid.so\n$(cat /etc/pam.d/sudo)" > /etc/pam.d/sudo
